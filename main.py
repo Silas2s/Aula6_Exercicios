60788c9490321fcefdd2f14de6b2b3ddf4a38dc1
Silas <145858740+Silas2s@users.noreply.github.com>
2023-10-07 15:30:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+# x = [1,3,6,9,845]
+
+# print(x,[1])
+
+# frutas = ['maça', 'banana', 'uva', 'cereja'.True]
+# nt(frutas[4])
+
+# frutas = ['maça', 'banana', 'uva', 'cereja', False]
+
+# add = frutas.append('melão')
+# print(frutas)
+# frutas.append(12)
+# print(frutas)
+
+# frutas = ['maça', 'banana', 'uva', 'cereja', True]
+# del frutas[0]
+# print(frutas)
+
+# frutas.remove('maça')
+# print(frutas)
+
+# x = len(frutas)
+# print(x)
+
+# for i in frutas:
+#   print(frutas)
+
+
+# Exercicio 01
+
+# numeros = ['1','2','3','4','5']
+# print(numeros)
+
+
+# Exercicio 02
+
+# elemento = numeros[2]
+# print(elemento)
+
+
+# Exercicio 03
+
+# numeros.append('6')
+# print(numeros)
+
+
+# Exercicio 04
+
+# numeros.remove('3')
+# print(numeros)
+
+
+# Exercicio 05
+
+# frutas = ['maça', 'banana', 'mamão']
+# print(numeros+frutas)
+
+# Exercicio 06
+
+# for elementos in frutas:
+#   print(frutas)
+
+
+#Exercicio 07
+
+# if '5' in numeros:
+#   print('O número 5 está na lista')
+# else:
+#   print('O número 5 não está na lista')
+
+# numero2 = [15,23,5,9,78,25]
+
+# numero = 25
+# if numero in numero2:
+#   print(f'existe o número {numero}')
+# else:
+#   print(f'Não existe {numero}')
